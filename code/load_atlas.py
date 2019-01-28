f399042005075b9f6cb2ae8ae50c94ab9add3494
Pablo Damasceno <pablodamasceno@gmail.com>
2019-01-28 13:00:28 -0800

    add example notebook

--- code/load_atlas.py
+++ code/load_atlas.py
@@ -1,4 +1,20 @@
 import pandas as pd
+import os
+
+def get_file_path(filename):
+    """Find filename in the relative directory `../data/` .
+
+    Args:
+        filename (str): file we're looking for in the ./data/ directory.
+
+    Returns:
+        str: absolute path to file "filename" in ./data/ dir.
+
+    """
+    here_dir = os.path.dirname(os.path.abspath(__file__))
+    file_dir = os.path.join(here_dir, '../data/', filename)
+
+    return file_dir
 
 def load_atlas(atlas="DK", portion="cortical"):
     """Load atlas data and returns it as a pandas dataframe.
@@ -12,8 +28,7 @@ def load_atlas(atlas="DK", portion="cortical"):
 
     """
     if atlas == "DK" and portion == "cortical":
-        atlas = pd.read_csv("../data/dk_cortical.csv")
+        filepath = get_file_path("dk_cortical.csv")
+        atlas = pd.read_csv(filepath)
 
     return atlas
-
-DK = load_atlas()
