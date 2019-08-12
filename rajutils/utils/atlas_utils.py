282b1d701cdad986393e5e7810d11383f70c0524
Pablo Damasceno <pablodamasceno@gmail.com>
2019-08-12 10:26:12 -0700

    add option to get portions of DK atlas

--- rajutils/utils/atlas_utils.py
+++ rajutils/utils/atlas_utils.py
@@ -30,29 +30,64 @@ def load_atlas(atlas="DK", portion="all"):
         atlas (pd.DataFrame): Database with atlas information.
 
     """
-    if atlas == "DK" and portion == "cortical":
-        filepath = get_file_path("dk_cortical.csv")
-        atlas_df = pd.read_csv(filepath)
-
-    elif atlas == "DK" and portion == "all":
+    if atlas == "DK":
         filepath = get_file_path("dk_all.csv")
-        atlas_df = pd.read_csv(filepath)
+        dk = pd.read_csv(filepath)
+
+        if portion == 'all':
+            indices = dk.index
+
+        else:
+
+            cortical_df = dk[dk['Cortex']=='cortical']
+            subcortical_df = dk[dk['Cortex']=='subcortical']
+
+            cort_L_idx = cortical_df[cortical_df['Hemisphere']=='Left'].index
+            cort_R_idx = cortical_df[cortical_df['Hemisphere']=='Right'].index
+
+            subcort_L_idx = subcortical_df[subcortical_df['Hemisphere']=='Left'].index
+            subcort_R_idx = subcortical_df[subcortical_df['Hemisphere']=='Right'].index
+
+            if portion == 'LR':
+                indices = list(cort_L_idx) + list(cort_R_idx)
+
+            elif portion == 'RL':
+                indices = list(cort_R_idx) + list(cort_L_idx)
+
+            elif portion == 'LRLR':
+                indices = list(cort_L_idx) + list(cort_R_idx)
+                indices += list(subcort_L_idx) + list(subcort_R_idx)
+
+            elif portion == 'LRRL':
+                indices = list(cort_L_idx) + list(cort_R_idx)
+                indices += list(subcort_R_idx) + list(subcort_L_idx)
 
     else:
-        print("atlas or portion not yet implemented.")
+        raise NameError("Atlas option not found.")
 
-    return atlas_df
+    return dk.loc[indices]
 
 def load_connectivity(atlas="DK", portion="all"):
 
     if atlas == "DK" and portion == "all":
         conn_filepath = get_file_path("dk_connectivity.mat")
         connectivity = pd.DataFrame(loadmat(conn_filepath)['meanACS'])
-        region_names = load_atlas(atlas="DK", portion="all").Name
+        region_names = load_atlas(atlas="DK", portion="all").Name[:86]
         connectivity.columns = list(region_names)
         connectivity.index = list(region_names)
 
         return(connectivity)
+
+def load_laplacian(n=0):
+
+    laplacian_filepath = get_file_path('laplacians.mat')
+    laplacians = pd.DataFrame(loadmat(laplacian_filepath)['laplacians'][0][0][n])
+    region_names = load_atlas(atlas="DK", portion="all").Name[:86]
+
+    laplacians.columns = list(region_names)
+    laplacians.index = list(region_names)
+
+    return(laplacians)
 
 def plot_glass_brains(color, coords, size):
 
